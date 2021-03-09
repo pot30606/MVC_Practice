05ab6952b31047dad4f4b04a6aeec7107abe2f3d
耀文 郭 <karta1407617@gmail.com>
2021-03-09 15:34:40 +0800

    建立Model , Service , Repository類別庫,並產生Migration 程式

--- MVC_Practice/Startup.cs
+++ MVC_Practice/Startup.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Models;
 
 namespace MVC_Practice
 {
@@ -22,6 +24,14 @@ namespace MVC_Practice
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //註冊 DB Context，指定使用 SQL 資料庫
+            services.AddDbContextPool<JournalDbContext>(options =>
+            {
+                //TODO: 實際應用時連線字串不該寫死，應移入設定檔並加密儲存
+                options.UseSqlServer(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = D:\DB.mdf; Integrated Security = True; Connect Timeout = 30");
+            });
+           
+
             services.AddControllersWithViews();
         }
 
